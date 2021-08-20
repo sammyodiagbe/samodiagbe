bf2709712d90e3eb73fcd09bc66c454e59725d62
odiagbe samson osaro <odiagbesamsonosaro@gmail.com>
2021-08-20 10:22:43 +0100

    restart from scratch

--- pages/index.js
+++ pages/index.js
@@ -1,95 +1,8 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { useState, useEffect } from "react";
-import { createPost, deletePost } from "../src/graphql/mutations";
-import { listPosts } from "../src/graphql/queries";
-import { API, graphqlOperation, withSSRContext, Auth } from "aws-amplify";
-import { onCreatePost, onDeletePost } from "../src/graphql/subscriptions";
 
-export default function Home({ posts: incomingPost }) {
-  const [postTitle, setPostTitle] = useState("");
-  const [postBody, setPostBody] = useState("");
-  const [posts, setPosts] = useState([...incomingPost]);
-
-  useEffect(() => {
-    const createusersubscription = API.graphql(
-      graphqlOperation(onCreatePost)
-    ).subscribe({
-      next: ({
-        _,
-        value: {
-          data: { onCreatePost },
-        },
-      }) => {
-        setPosts((posts) => [...posts, onCreatePost]);
-      },
-      error: (error) => console.log(error),
-    });
-
-    const deletepostsubscription = API.graphql(
-      graphqlOperation(onDeletePost)
-    ).subscribe({
-      next: ({
-        _,
-        value: {
-          data: { onDeletePost },
-        },
-      }) => {
-        const { id } = onDeletePost;
-        const newPost = [...posts].filter((post) => {
-          return post.id !== id;
-        });
-        setPosts(newPost);
-      },
-      error: (err) => console.log("Something went wrong, ", err),
-    });
-
-    return () => {
-      createusersubscription.unsubscribe();
-      deletepostsubscription.unsubscribe();
-    };
-  }, []);
-
-  const createNewPost = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await API.graphql(
-        graphqlOperation(createPost, { input: { postTitle, postBody } })
-      );
-      setPostTitle("");
-      setPostBody("");
-    } catch (err) {
-      console.log("Something went wrong, ", err);
-    }
-  };
-
-  const deletePost_ = async (id) => {
-    try {
-      const deletepost = await API.graphql(
-        graphqlOperation(deletePost, { input: { id: id } })
-      );
-    } catch (err) {
-      console.log("Something went wrong while trying to delete post, ", err);
-    }
-  };
-
-  const renderPosts = posts.length ? (
-    posts
-      .sort((a, b) => b.createdAt.localeCompare(a.createAt))
-      .map((post, index) => {
-        return (
-          <div className={styles.apost} key={index}>
-            <h2>{post.postTitle}</h2>
-            <p>{post.postBody}</p>
-            <div>
-              <button onClick={() => deletePost_(post.id)}>Delete post</button>
-            </div>
-          </div>
-        );
-      })
-  ) : (
-    <p>No posts oo</p>
-  );
+export default function Home() {
+  
   return (
     <div className={styles.container}>
       <Head>
@@ -97,32 +10,7 @@ export default function Home({ posts: incomingPost }) {
         <meta name="description" content="Sam Odiagbe blog" />
         <meta name="author" content="Sam Odiagbe" />
       </Head>
-      <div className={styles.contentContainer}>
-        <div className={styles.createPostContainer}>
-          {/* <AmplifySignOut /> */}
-          <form onSubmit={createNewPost}>
-            <div>
-              <input
-                type="text"
-                placeholder="Post title"
-                onChange={(e) => setPostTitle(e.target.value)}
-                value={postTitle}
-              />
-            </div>
-            <div>
-              <textarea
-                placeholder="Post body"
-                onChange={(e) => setPostBody(e.target.value)}
-                value={postBody}
-              />
-            </div>
-            <div>
-              <button>Create post</button>
-            </div>
-          </form>
-        </div>
-        <div className={styles.postsContainer}>{renderPosts}</div>
-      </div>
+      
     </div>
   );
 }
@@ -142,23 +30,23 @@ export default function Home({ posts: incomingPost }) {
 //   };
 // };
 
-export const getStaticProps = async (ctx) => {
-  const { API } = withSSRContext(ctx);
-  let posts = [];
-  try {
-    const res = await API.graphql({ query: listPosts, authMode: "AWS_IAM" });
-    const {
-      listPosts: { items },
-    } = res.data;
-    posts = items;
-  } catch (err) {
-    console.log("There was an error, ", err);
-  }
+// export const getStaticProps = async (ctx) => {
+//   const { API } = withSSRContext(ctx);
+//   let posts = [];
+//   try {
+//     const res = await API.graphql({ query: listPosts, authMode: "AWS_IAM" });
+//     const {
+//       listPosts: { items },
+//     } = res.data;
+//     posts = items;
+//   } catch (err) {
+//     console.log("There was an error, ", err);
+//   }
 
-  return {
-    props: {
-      posts,
-    },
-    // revalidate: 1,
-  };
-};
+//   return {
+//     props: {
+//       posts,
+//     },
+//     // revalidate: 1,
+//   };
+// };
