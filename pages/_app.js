772a1c3adfd84adf811cb814b21c47af73a45806
odiagbe samson osaro <odiagbesamsonosaro@gmail.com>
2021-08-20 10:24:13 +0100

    trimming code down

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,7 @@
 import "../styles/globals.css";
-import Amplify from "aws-amplify";
-import awsConfig from "../aws-exports";
-import { withAuthenticator } from "@aws-amplify/ui-react";
-
-Amplify.configure({ ...awsConfig, ssr: true });
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-export default withAuthenticator(MyApp);
+export default (MyApp);
