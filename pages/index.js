552a6bce8b27d4cb6ccf4dc73b95ecb42272409d
odiagbe samson osaro <odiagbesamsonosaro@gmail.com>
2021-08-23 21:59:34 +0100

    change meta data

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,13 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  
   return (
     <div className={styles.container}>
       <Head>
         <title>Sammy Odiagbe</title>
-        <meta name="description" content="Sam Odiagbe blog" />
+        <meta name="description" content="Sam Odiagbe" />
         <meta name="author" content="Sam Odiagbe" />
       </Head>
-      
     </div>
   );
 }
